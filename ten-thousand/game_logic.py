f03ee67dcb4c283b6e97e862a333ea781201bcba
Alex Guo <guojiarui@gmail.com>
2022-07-26 23:57:08 -0700

    sim test functions added. not fully work.

--- ten-thousand/game_logic.py
+++ ten-thousand/game_logic.py
@@ -1,5 +1,13 @@
-import random
+from random import randint
 from collections import Counter
+
+
+def default_dice_roller():
+    return randint(1, 6), randint(1, 6)
+
+
+def dice_roller_4_3():
+    return 4, 3
 
 
 class GameLogic:
@@ -10,63 +18,54 @@ class GameLogic:
     def roll_dice(value):
         dice_list = []
         while len(dice_list) != value:
-            dice_list.append(random.randint(1, 6))
+            dice_list.append(randint(1, 6))
         return tuple(dice_list)
 
     @staticmethod
-    def calculate_score(roll, pattern=None):
-        score_pattern = [
-            (tuple(), 0),
-            ((1,), 100),
-            ((1, 1), 200),
-            ((1, 1, 1), 1000),
-            ((1, 1, 1, 1), 2000),
-            ((1, 1, 1, 1, 1), 3000),
-            ((1, 1, 1, 1, 1, 1), 4000),
-            ((2,), 0),
-            ((2, 2), 0),
-            ((2, 2, 2), 200),
-            ((2, 2, 2, 2), 400),
-            ((2, 2, 2, 2, 2), 600),
-            ((2, 2, 2, 2, 2, 2), 800),
-            ((3,), 0),
-            ((3, 3), 0),
-            ((3, 3, 3), 300),
-            ((3, 3, 3, 3), 600),
-            ((3, 3, 3, 3, 3), 900),
-            ((3, 3, 3, 3, 3, 3), 1200),
-            ((4,), 0),
-            ((4, 4), 0),
-            ((4, 4, 4), 400),
-            ((4, 4, 4, 4), 800),
-            ((4, 4, 4, 4, 4), 1200),
-            ((4, 4, 4, 4, 4, 4), 1600),
-            ((5,), 50),
-            ((5, 5), 100),
-            ((5, 5, 5), 500),
-            ((5, 5, 5, 5), 1000),
-            ((5, 5, 5, 5, 5), 1500),
-            ((5, 5, 5, 5, 5, 5), 2000),
-            ((6,), 0),
-            ((6, 6), 0),
-            ((6, 6, 6), 600),
-            ((6, 6, 6, 6), 1200),
-            ((6, 6, 6, 6, 6), 1800),
-            ((6, 6, 6, 6, 6, 6), 2400),
-            ((1, 2, 3, 4, 5, 6), 1500),
-            ((2, 2, 3, 3, 4, 6), 0),
-            ((2, 2, 3, 3, 6, 6), 1500),
-            ((1, 1, 1, 2, 2, 2), 1200),
-            ((1, 1, 1, 3, 3, 3), 1300),
-            ((1, 1, 1, 4, 4, 4), 1400),
-            ((1, 1, 1, 5, 5, 5), 1500),
-            ((1, 1, 1, 6, 6, 6), 1500),
-        ]
-        while pattern in score_pattern:
-            if pattern[0] == roll:
-                return pattern[1]
-        return 0
+    def calculate_score(dice):  # dice is a tuple of ints from user dice roll
+        if len(dice) > 6:
+            raise Exception
+        counts = Counter(dice)
+        if len(counts) == 6:  # 1, 2, 3, 4, 5, 6
+            return 1500
+        if len(counts) == 3 and all(val == 2 for val in counts.values()):
+            return 1500
+        score = 0
+        ones_used = False
+        fives_used = False
+        for num in range(1, 7):
+            a_count = counts[num]
+            if a_count >= 3:
+                if num == 1:
+                    ones_used = True
+                elif num == 5:
+                    fives_used = True
+                    score += num * 100
+                    # handle 4,5,6 of a kind
+                    a_beyond_3 = a_count - 3
+                    score += score * a_beyond_3
+                    if num == 1:
+                        score *= 100
+        if not ones_used:
+            score += counts.get(1, 0) * 100
+        if not fives_used:
+            score += counts.get(5, 0) * 50
+        return score
 
+    def play_dice(self):
+        while True:
+            print("Enter r to roll or q to quit")
+            choice = input("> ")
+            if choice == "q":
+                print("OK, bye")
+                break
+            elif choice == "r":
+                roll = self()
+                values_in_roll = []
+                for value in roll:
+                    values_in_roll.append(str(value))
+                formatted_roll = " ".join(values_in_roll)
+                print(f"*** {formatted_roll} ***")
 
-
-
+    if __name__ == '__main__':
+        play_dice(dice_roller_4_3)
